c88391a3f052a32482fd158361bf97a4c9912fc4
vnordin <vnordin@se.ibm.com>
2018-11-12 13:35:51 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+// app.get('/', (req, res) => {
+//     //nedan är vad någon kommer få när man besöker sidan, eller om en request kommer från en app kommer nedan string finnas i bodyn.
+//     //res.send('<h1>hello express</h1>');
+//     res.send({
+//         name: 'Victoria',
+//         likes: [
+//             'Cykel',
+//             'Cities'
+//         ]
+//     });
+// });
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Victorias Home page',
+        welcomeMessage: 'Welcome to my page!'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'Victorias about Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Could not fulfill request.'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+});
