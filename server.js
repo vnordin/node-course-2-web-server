794a1157acae05c039fc7e07a5871b911aa30baa
vnordin <vnordin@se.ibm.com>
2018-11-12 14:21:48 +0100

    add project page

--- server.js
+++ server.js
@@ -60,6 +60,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Could not fulfill request.'
